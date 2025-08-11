d7e4f830bd87e53c2683933f8944fe54cf9384da
Kelia Iradukunda <iradukundakelia@gmail.com>
2025-08-11 09:38:13 +0200

    Create a form component

---
+++ src/components/Form.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Form = () => {
+
+  return (
+
+    <form action="">
+        <div className="mb-3 gap-3 rounded-lg">
+            <label htmlFor="name"></label>
+            <input id='name' type="text" />
+        </div>
+    </form>
+  )
+}
+
+export default Form
